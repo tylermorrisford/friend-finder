a283d7a49bc4629d4f425140c361bfbf9eb94173
tylermorrisford <tylerford@gmail.com>
2019-09-29 19:29:51 -0600

    add server and get route

--- server.js
+++ server.js
@@ -7,3 +7,12 @@ var PORT = 8080;
 // setup express to return json
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.get("/", function(req, res){
+    res.send('teef this site is killer, my buddd!');
+})
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
