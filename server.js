5c282618a2a85735fe17acbb5c3cc33ea9d9ed99
tylermorrisford <tylerford@gmail.com>
2019-09-30 22:54:39 -0600

    add routes/update style

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 // friend-finder
 const express = require('express');
 const path =  require('path');
+// require files --- research these
+// var htmlRoutes = require("./app/routing/htmlRoutes");
+// var apiRoutes = require("./app/routing/apiRoutes");
 
 var app = express();
-var PORT = 8080;
+var PORT = process.env.PORT || 8080;
 // setup express to return json
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", function(req, res){
-    res.send('teef this site is killer, my buddd!');
+    res.sendFile(path.join(__dirname, "app/public/home.html"));
+})
+
+app.get("/survey", function(req, res){
+    res.sendFile(path.join(__dirname, "app/public/survey.html"));
 })
 
 
