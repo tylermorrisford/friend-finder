4f8eb44965f974acbd2f97141d8b3990142208e4
tylermorrisford <tylerford@gmail.com>
2019-10-02 20:13:50 -0600

    post request returns data without ajax

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // friend-finder
 const express = require('express');
-const path =  require('path');
+const path = require('path');
+const bodyParser = require('body-parser');
 // require files --- research these
 // var htmlRoutes = require("./app/routing/htmlRoutes");
 // var apiRoutes = require("./app/routing/apiRoutes");
@@ -9,7 +10,12 @@ var app = express();
 var PORT = process.env.PORT || 8080;
 // setup express to return json
 app.use(express.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.json());
+
+// data
+var friends = [];
+
 
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "app/public/home.html"));
@@ -19,7 +25,17 @@ app.get("/survey", function(req, res){
     res.sendFile(path.join(__dirname, "app/public/survey.html"));
 })
 
+app.post("/survey", function(req, res){
+    // console.log(req);
+    // var newName = req.body;
+    // console.log(req.body);
+    // console.log(req);
+    var newFriend = req.body;
+    friends.push(newFriend);
+    console.log(friends);
+    res.end();
+});
 
 app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    console.log("App listening on PORT " + PORT + ". no really, I'm listening");
 });
