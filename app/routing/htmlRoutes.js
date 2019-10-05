53ef45501904b94f6e961526ed03f64bb93322b4
tylermorrisford <tylerford@gmail.com>
2019-10-05 01:02:14 -0600

    add links for friends json & repo

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -7,7 +7,7 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
     
-    // catch-all for home route
+    // catch-all for home route, loaded after specific route; required in server after api routes
     app.get("*", function(req, res){
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
